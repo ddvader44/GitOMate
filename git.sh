07575ae2f4e80e622ffb926d973c9e1ab93df31c
Dishit <40226856+ddvader44@users.noreply.github.com>
2022-09-24 01:53:22 +0530

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#go to the folder you wanna work on 
+cd ~/Documents/GitOMate
+
+git add .
+
+DATE = $(date) 
+
+git commit -m "changes made on $DATE"
+
+git push
+
+#show desktop notification according to your OS
+UNAME=$(uname)
+
+if [ "$UNAME" == "Linux" ] ; then
+	notify-send "pushed to remote!"
+elif [ "$UNAME" == "Darwin" ] ; then
+	osascript -e 'display notification "pushed to remote" with title "Success"'
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	echo "pushed to remote!"
+fi
